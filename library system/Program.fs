01a771f73e1f7867b17aeaf25614932c45e4e0f0
Youssef tarek <yossef.tarekelsheikh@gmail.com>
2024-12-09 18:18:58 +0200

    Add project files.

---
+++ library system/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+open System.Windows.Forms
+open System.Drawing
+
+// Book Record Type
+type Book = {
+    Title: string
+    Author: string
+    Genre: string
+    IsBorrowed: bool
+    BorrowDate: DateTime option
+}
+
+// Initial Library Data
+let mutable library = [
+    { Title = "Book 1"; Author = "Author 1"; Genre = "Fiction"; IsBorrowed = false; BorrowDate = None }
+    { Title = "Book 2"; Author = "Author 2"; Genre = "Science"; IsBorrowed = true; BorrowDate = Some(DateTime.Now.AddDays(-5.0)) }
+]
